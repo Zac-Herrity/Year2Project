b35c18fd8912dd41d42d669d9e785717b58ebeb6
S00269936 Zac <S00269936@atu.ie>
2025-12-17 10:09:42 +0000

    fixed regex and added input validation for payment.js

--- public/payment.js
+++ public/payment.js
@@ -7,8 +7,25 @@ document.getElementById('payment').addEventListener('submit', function(){
     const cvv = document.getElementById('cvv').value.trim();
 
     //regex
-    const regexName = /^[A-Za-z]+([ .][A-Za-z]+)*$/;
+    const regexName = /^[a-zA-z]{3,12} [a-zA-z]{2,12}$/;
     const regexNumber = /^\d{16}$/;
     const regexExp = /^(0[1-9]|1[0-2])\/\d{2}$/;
     const regexCVV = /^\d{3}$/;
+
+    if(!regexName.test(name)){
+        alert('Please enter a valid name');
+        return;
+    }
+    if(!regexNumber.test(number)) {
+        alert('Card number must be 16 digits long');
+        return;
+    }
+    if(!regexExp.test(exp)){
+        alert('Expiary date must match MM/YY');
+        return;
+    }
+    if(!regexCVV.test(cvv)){
+        alert('Must be 3 digits');
+        return;
+    }
 })
