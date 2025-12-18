44c7b1477939a9a829073bf6bbc8f69dc9ad4444
S00269936 Zac <S00269936@atu.ie>
2025-12-18 11:17:10 +0000

    started login features

--- public/account.js
+++ public/account.js
@@ -9,3 +9,13 @@ if(!localStorage.getItem('users')){
     ];
     localStorage.setItem('users', JSON.stringify(users));
 }
+
+// #region Login
+document.getElementById('loginForm').addEventListener('submit', function(x){
+    x.preventDefault();
+
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const users = JSON.parse(localStorage.getItem('users')) || []; //parses local storage OR returns empty array
+})
+//#endregion
