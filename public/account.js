5410f3b6194ef0e4dbc42d2ba33afaa5aa67b0fe
S00269936 Zac <S00269936@atu.ie>
2025-12-18 11:34:03 +0000

    log in js completed, testing needed

--- public/account.js
+++ public/account.js
@@ -17,5 +17,23 @@ document.getElementById('loginForm').addEventListener('submit', function(x){
     const email = document.getElementById('email').value.trim();
     const password = document.getElementById('password').value.trim();
     const users = JSON.parse(localStorage.getItem('users')) || []; //parses local storage OR returns empty array
-})
+
+    let foundUser = null;
+
+    for (let i = 0; i < users.length; i++) {
+    if (users[i].email === email && users[i].password === password) { //loops through users to check if details match those in json
+    foundUser = users[i];
+    console.log('searching for match');
+    break;
+  }
+}
+if(!foundUser){
+    alert('Invalid Login');
+    console.log('login did not match');
+    return;
+}
+
+localStorage.setItem(userLoggedIn, foundUser.name);
+
+});
 //#endregion
